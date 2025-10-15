760dc94c18cc8e016b650f2a31a67ec1fe2b2e3c
matteonobyai-hue <matteonobyai@gmail.com>
2025-10-15 13:03:26 +0200

    Update get-upload-url.js

--- functions/api/get-upload-url.js
+++ functions/api/get-upload-url.js
@@ -1,32 +1,32 @@
 // functions/api/get-upload-url.js
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { AwsClient } from 'aws4fetch'
 
-export async function onRequestPost(context) {
-  const { request } = context;
-  const body = await request.json();
+export async function onRequestPost({ request }) {
+  const { fileName, fileType } = await request.json()
 
-  const { fileName, fileType } = body;
+  const bucket = 'videos-driver'
+  const region = 'auto'
+  const accountId = 'a20f4c50d50b88091cc283a249544f43'
+  const endpoint = `https://${accountId}.r2.cloudflarestorage.com`
 
-  const s3 = new S3Client({
-    region: "auto",
-    endpoint: "https://a20f4c50d50b88091cc283a249544f43.r2.cloudflarestorage.com",
-    credentials: {
-      accessKeyId: "3c246b59f0f8bd24fc62e14addf0dba1",
-      secretAccessKey: "5e6504bca6543a01e00184cac86c746f73b1afac8ecfbfcbd238575b6d21f73"
+  const url = `${endpoint}/${bucket}/${fileName}`
+
+  const client = new AwsClient({
+    accessKeyId: '3c246b59f0f8bd24fc62e14addf0dba1',
+    secretAccessKey: '5e6504bca6543a01e00184cac86c746f73b1afac8ecfbfcbd238575b6d21f73',
+    service: 's3',
+    region
+  })
+
+  const signed = await client.sign(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': fileType
     }
-  });
+  })
 
-  const command = new PutObjectCommand({
-    Bucket: "videos-driver",
-    Key: fileName,
-    ContentType: fileType,
-  });
-
-  const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-  return new Response(JSON.stringify({ uploadUrl }), {
-    headers: { "Content-Type": "application/json" }
-  });
+  return new Response(JSON.stringify({ uploadUrl: signed.url }), {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
